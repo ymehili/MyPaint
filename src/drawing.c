9015d6fe6f467082669bdc665bcd8ae173590f60
ymehili <youssefmehili@gmail.com>
2024-03-15 13:15:02 +0100

    Refactor set_pixel_if_edge function and remove unnecessary code

--- src/drawing.c
+++ src/drawing.c
@@ -60,19 +60,6 @@ static sfVector2i interpolate_position(global_t *global, drawing_t *drw, int i)
     return interpolatedPos;
 }
 
-static void set_pixel_if_edge(global_t *global, drawing_t *drw, int dx,
-    int dy)
-{
-    sfFloatRect bounds = {drw->spritePos.x, drw->spritePos.y,
-        drw->textureSize.x * drw->scale.x, drw->textureSize.y * drw->scale.y};
-
-    if (0 <= drw->interpolatedPos.x + dx && drw->interpolatedPos.x
-        + dx < bounds.width && 0 <= drw->interpolatedPos.y
-        + dy && drw->interpolatedPos.y + dy < bounds.height)
-        sfImage_setPixel(drw->image, drw->interpolatedPos.x + dx,
-            drw->interpolatedPos.y + dy, drw->color);
-}
-
 static void draw_circle(global_t *global, drawing_t *drw)
 {
     int dx;
@@ -81,8 +68,18 @@ static void draw_circle(global_t *global, drawing_t *drw)
     for (int angle = 0; angle < 360; angle++) {
         dx = global->pencil->size * cos(angle * M_PI / 180.0);
         dy = global->pencil->size * sin(angle * M_PI / 180.0);
-        set_pixel_if_edge(global, drw, dx, dy);
+        sfImage_setPixel(drw->image, drw->interpolatedPos.x + dx,
+            drw->interpolatedPos.y + dy, drw->color);
     }
+}
+
+static void set_pixel_if_edge(global_t *global, drawing_t *drw, int dx,
+    int dy)
+{
+    if (dx == -global->pencil->size || dx == global->pencil->size ||
+        dy == -global->pencil->size || dy == global->pencil->size)
+        sfImage_setPixel(drw->image, drw->interpolatedPos.x + dx,
+            drw->interpolatedPos.y + dy, drw->color);
 }
 
 static void draw_square(global_t *global, drawing_t *drw)
